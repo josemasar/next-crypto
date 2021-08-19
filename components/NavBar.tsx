ad98c7991bf6a0afcddf92af158319c44cfd151f
josemasar <josemanuelsarrion@gmail.com>
2021-08-19 20:44:02 +0200

    added navbar, date and componetization

---
+++ components/NavBar.tsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return(
+        <nav className="max-w-8xl mx-auto px-4 bg-black h-16">
+        <ul className="flex flex-auto justify-center">
+          <li className="text-white py-3 font-bold text-2xl">Crypto Next</li>
+        </ul>
+      </nav>
+    )
+}
+
+export default NavBar
--- components/TableCoins.tsx
+++ components/TableCoins.tsx
@@ -1,28 +1,35 @@
 import Image from 'next/image'
 
-const TableCoins = ( {coins} ) => {
+interface CoinType {
+    [key: string]: any
+  }
+
+
+const TableCoins = ( { coins } : CoinType ) => {  
 
     return(
-            <table>
-            <thead>
-            <tr>
-                <th>Name</th>
-                <th>Image</th>
-                <th>Symbol</th>
-                <th>Current price</th>
-            </tr>
-            </thead>
-            <tbody>
-             {coins.map( (coin, index) => (
-                <tr key={index}>
-                    <td>{coin.name}</td>
-                    <td>{coin.symbol}</td>
-                    <td><Image src={coin.image} alt={coin.name} width={20} height={20} /></td>
-                    <td>{coin.current_price}</td>
+        <div className="max-w-5xl mx-auto flex flex-row justify-center sm:rounded-lg">
+            <table className="table-auto min-w-full border-2 border-gray-200">
+                <thead className="bg-blue-200 bg-opacity-50">
+                <tr>
+                    <th className="px-6 py-3 text-xl text-center tracking-wider">Name</th>
+                    <th className="px-6 py-3 text-xl text-center tracking-wider">Symbol</th>
+                    <th className="px-6 py-3 text-xl text-center tracking-wider">Image</th>
+                    <th className="px-6 py-3 text-xl text-center tracking-wider">Current price</th>
                 </tr>
-             ))} 
-            </tbody>
-        </table>
+                </thead>
+                <tbody className="divide-y divide-gray-200">
+                {coins.map( (coin: any, index: number) => (
+                    <tr key={index} className="hover:bg-indigo-100">
+                        <td className="text-center font-semibold py-4">{coin.name}</td>
+                        <td className="text-center text-gray-500">{coin.symbol}</td>
+                        <td className="text-center"><Image src={coin.image} alt={coin.name} width={32} height={32} /></td>
+                        <td className="text-center">{coin.current_price}</td>
+                    </tr>
+                ))} 
+                </tbody>
+            </table>
+        </div>
     )
 }
 
